fd74dc6c07a926d072d4e2f6c9422b3f5edf83c3
Jakub Ledwon <leedwon12@gmail.com>
2025-06-02 21:49:54 +0200

    Add logic to GameViewModel

--- app/src/main/kotlin/com/ledwon/jakub/nqueens/features/game/GameViewModel.kt
+++ app/src/main/kotlin/com/ledwon/jakub/nqueens/features/game/GameViewModel.kt
@@ -2,18 +2,25 @@ package com.ledwon.jakub.nqueens.features.game
 
 import androidx.compose.runtime.Stable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ledwon.jakub.nqueens.core.game.Conflicts
+import com.ledwon.jakub.nqueens.core.game.GameEngineFactory
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.collections.immutable.persistentMapOf
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.toPersistentMap
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import com.ledwon.jakub.nqueens.core.game.BoardPosition as CoreBoardPosition
 
 @Stable
 @HiltViewModel(assistedFactory = GameViewModel.Factory::class)
 class GameViewModel @AssistedInject constructor(
-    @Assisted private val boardSize: Int
+    @Assisted private val boardSize: Int,
+    gameEngineFactory: GameEngineFactory
 ) : ViewModel() {
 
     @AssistedFactory
@@ -21,13 +28,62 @@ class GameViewModel @AssistedInject constructor(
         fun create(boardSize: Int): GameViewModel
     }
 
-    private val _state = MutableStateFlow(createInitialState())
-    val state = _state.asStateFlow()
+    private val gameEngine = gameEngineFactory.create(boardSize = boardSize)
+
+    val state = gameEngine.state.map {
+        GameState(
+            boardSize = boardSize,
+            cells = createCells(
+                queens = it.queens,
+                conflicts = it.conflicts
+            )
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = createInitialState()
+    )
+
+    fun onCellClick(position: BoardPosition) {
+        gameEngine.toggleQueen(position = position.toCorePosition())
+    }
+
+    private fun createCells(
+        queens: Set<CoreBoardPosition>,
+        conflicts: Conflicts
+    ): ImmutableMap<BoardPosition, Cell> {
+        return (0 until boardSize).flatMap { row ->
+            (0 until boardSize).map { column ->
+                val position = CoreBoardPosition(row, column)
+                val cell = Cell(
+                    hasQueen = position in queens,
+                    hasConflict = position in conflicts
+                )
+                position.toUiPosition() to cell
+            }
+        }
+            .toMap()
+            .toPersistentMap()
+    }
 
     private fun createInitialState(): GameState {
         return GameState(
             boardSize = boardSize,
-            cells = persistentMapOf()
+            cells = createCells(
+                queens = emptySet(),
+                conflicts = Conflicts(
+                    rows = emptySet(),
+                    columns = emptySet(),
+                    majorDiagonals = emptySet(),
+                    minorDiagonals = emptySet()
+                )
+            )
         )
     }
+
+    private fun CoreBoardPosition.toUiPosition(): BoardPosition =
+        BoardPosition(row = row, column = column)
+
+    private fun BoardPosition.toCorePosition(): CoreBoardPosition =
+        CoreBoardPosition(row = row, column = column)
 }
--- app/src/test/kotlin/com/ledwon/jakub/nqueens/features/game/GameViewModelTest.kt
+++ app/src/test/kotlin/com/ledwon/jakub/nqueens/features/game/GameViewModelTest.kt
@@ -1,20 +1,205 @@
 package com.ledwon.jakub.nqueens.features.game
 
+import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
-import kotlinx.collections.immutable.persistentMapOf
+import com.ledwon.jakub.nqueens.core.game.GameEngine
+import com.ledwon.jakub.nqueens.core.game.GameEngineFactory
+import kotlinx.collections.immutable.toPersistentMap
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class GameViewModelTest {
 
-    private val viewModel = GameViewModel(boardSize = 8)
+    private val boardSize = 4
+    private val gameEngineFactory = object : GameEngineFactory {
+        override fun create(boardSize: Int): GameEngine {
+            return GameEngine(boardSize = boardSize)
+        }
+    }
+    private val viewModel = GameViewModel(
+        boardSize = boardSize,
+        gameEngineFactory = gameEngineFactory
+    )
 
     @Test
     fun `has correct initial state`() {
         val expectedState = GameState(
-            boardSize = 8,
-            cells = persistentMapOf<BoardPosition, Cell>()
+            boardSize = boardSize,
+            cells = emptyCells().toPersistentMap()
         )
 
         assertThat(viewModel.state.value).isEqualTo(expectedState)
+    }
+
+    @Test
+    fun `adds queen on cell click`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            val position = BoardPosition(row = 0, column = 0)
+            viewModel.onCellClick(position)
+
+            val expected = emptyCells()
+                .apply { set(position, Cell(hasQueen = true, hasConflict = false)) }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expected)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `removes queen on cell click when already present`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            val position = BoardPosition(row = 0, column = 0)
+            viewModel.onCellClick(position)
+
+            val expected = emptyCells()
+                .apply { set(position, Cell(hasQueen = true, hasConflict = false)) }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expected)
+
+            viewModel.onCellClick(position)
+            assertThat(awaitItem().cells).isEqualTo(emptyCells())
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `creates cells with row conflict`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            viewModel.onCellClick(BoardPosition(row = 0, column = 0))
+            viewModel.onCellClick(BoardPosition(row = 0, column = 1))
+
+            val expectedCells = emptyCells()
+                .apply {
+                    set(BoardPosition(row = 0, column = 0), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 0, column = 1), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 0, column = 2), Cell(hasQueen = false, hasConflict = true))
+                    set(BoardPosition(row = 0, column = 3), Cell(hasQueen = false, hasConflict = true))
+                }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expectedCells)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `creates cells with column conflict`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            viewModel.onCellClick(BoardPosition(row = 0, column = 0))
+            viewModel.onCellClick(BoardPosition(row = 1, column = 0))
+
+            val expectedCells = emptyCells()
+                .apply {
+                    set(BoardPosition(row = 0, column = 0), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 1, column = 0), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 2, column = 0), Cell(hasQueen = false, hasConflict = true))
+                    set(BoardPosition(row = 3, column = 0), Cell(hasQueen = false, hasConflict = true))
+                }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expectedCells)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `creates cells with major diagonal conflict`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            viewModel.onCellClick(BoardPosition(row = 0, column = 0))
+            viewModel.onCellClick(BoardPosition(row = 1, column = 1))
+
+            val expectedCells = emptyCells()
+                .apply {
+                    set(BoardPosition(row = 0, column = 0), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 1, column = 1), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 2, column = 2), Cell(hasQueen = false, hasConflict = true))
+                    set(BoardPosition(row = 3, column = 3), Cell(hasQueen = false, hasConflict = true))
+                }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expectedCells)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `creates cells with minor diagonal conflict`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            viewModel.onCellClick(BoardPosition(row = 0, column = 3))
+            viewModel.onCellClick(BoardPosition(row = 1, column = 2))
+
+            val expectedCells = emptyCells()
+                .apply {
+                    set(BoardPosition(row = 0, column = 3), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 1, column = 2), Cell(hasQueen = true, hasConflict = true))
+                    set(BoardPosition(row = 2, column = 1), Cell(hasQueen = false, hasConflict = true))
+                    set(BoardPosition(row = 3, column = 0), Cell(hasQueen = false, hasConflict = true))
+                }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expectedCells)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `creates cells with no conflicts when queens are placed correctly`() = runTest {
+        viewModel.state.test {
+            awaitItem() // Initial state
+
+            val positions = listOf(
+                BoardPosition(0, 1),
+                BoardPosition(1, 3),
+                BoardPosition(2, 0),
+                BoardPosition(3, 2)
+            )
+
+            positions.forEach { viewModel.onCellClick(it) }
+
+            val expectedCells = emptyCells()
+                .apply {
+                    positions.forEach { set(it, Cell(hasQueen = true, hasConflict = false)) }
+                }
+                .toPersistentMap()
+
+            assertThat(awaitItem().cells).isEqualTo(expectedCells)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    private fun emptyCell() = Cell(
+        hasQueen = false,
+        hasConflict = false
+    )
+
+    private fun emptyCells(): MutableMap<BoardPosition, Cell> {
+        return (0 until boardSize).flatMap { row ->
+            (0 until boardSize).map { column ->
+                BoardPosition(row, column) to emptyCell()
+            }
+        }
+            .toMap()
+            .toMutableMap()
     }
 }
