c5651338d1c04cb4d574d0ecd8c154b95caabb84
Jakub Ledwon <leedwon12@gmail.com>
2025-06-08 20:05:02 +0200

    These are better suited for e2e tests

--- app/src/androidTest/kotlin/com/ledwon/jakub/nqueens/tests/mainmenu/MainMenuTests.kt
+++ app/src/androidTest/kotlin/com/ledwon/jakub/nqueens/tests/mainmenu/MainMenuTests.kt
@@ -1,8 +1,6 @@
 package com.ledwon.jakub.nqueens.tests.mainmenu
 
 import com.ledwon.jakub.nqueens.BaseTestCase
-import com.ledwon.jakub.nqueens.tests.game.GameRobot
-import com.ledwon.jakub.nqueens.tests.leaderboard.LeaderboardRobot
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Test
 
@@ -19,27 +17,5 @@ class MainMenuTests : BaseTestCase() {
             .assertBoardSize("4 x 4")
             .setBoardSize(8f)
             .assertBoardSize("8 x 8")
-    }
-
-    @Test
-    fun opensGame() {
-        val robot = MainMenuRobot(composeTestRule)
-        val gameRobot = GameRobot(composeTestRule)
-
-        robot
-            .setBoardSize(5f)
-            .clickPlayButton()
-
-        gameRobot.assertBoardSizeIs(5)
-    }
-
-    @Test
-    fun opensLeaderboard() {
-        val robot = MainMenuRobot(composeTestRule)
-        val leaderboardRobot = LeaderboardRobot(composeTestRule)
-
-        robot.clickLeaderboardButton()
-
-        leaderboardRobot.assertTopBarTitleIsDisplayed()
     }
 }
